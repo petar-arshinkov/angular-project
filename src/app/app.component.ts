2ed8f8a3ed656af10c184d289e197ee9949fbca6
petar-arshinkov <143599367+petar-arshinkov@users.noreply.github.com>
2024-12-05 21:03:57 +0200

    reuploaded files due to error in using Git

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,AuthenticateComponent,],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'StocksProject';
+}
---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { emailValidator } from '../../utils/email.validator';
+import { DOMAINS } from '../../constants';
+import { matchPasswordsValidator } from '../../utils/match-passwords.validator';
+// import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    email: new FormControl('', [Validators.required, emailValidator(DOMAINS)]),
+    passGroup: new FormGroup(
+      {
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(5),
+        ]),
+        rePassword: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      }
+    ),
+  });
+
+  constructor( private router: Router) {}
+
+  isFieldTextMissing(controlName: string) {
+    return (
+      this.form.get(controlName)?.touched &&
+      this.form.get(controlName)?.errors?.['required']
+    );
+  }
+
+  get isNotMinLength() {
+    return (
+      this.form.get('username')?.touched &&
+      this.form.get('username')?.errors?.['minlength']
+    );
+  }
+
+  get isEmailNotValid() {
+    return (
+      this.form.get('email')?.touched &&
+      this.form.get('email')?.errors?.['emailValidator']
+    );
+  }
+
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
+
+  register() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const {
+      username,
+      email,
+      passGroup: { password, rePassword } = {},
+    } = this.form.value;
+
+    console.log(this.form);
+    
+
+    // this.userService
+    //   .register(username!, email!, password!, rePassword!)
+    //   .subscribe(() => {
+    //     this.router.navigate(['/home']);
+    //   });
+  }
+}
